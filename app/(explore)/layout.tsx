509e032dace780283fdee0f1d997b675d0c9970f
Bazarin <bazarintechnologies@gmail.com>
2025-04-02 04:19:50 +0300

    Fix: Removed un used variable to improve perfomance and resolve compilation errors at production

--- app/(explore)/layout.tsx
+++ app/(explore)/layout.tsx
@@ -1,19 +1,5 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 import Topbar2 from "@/components/Topbar2";
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 export default function RootLayout({
   children,
