9a8c11198f5157eeaecce3bb38efcc6ed05fc9cb
Bazarin <bazarintechnologies@gmail.com>
2025-04-03 19:34:44 +0300

    Style: adjusted team component so that to avoid unnecerary overflow. so I had to trim an email

--- components/TeamComp.tsx
+++ components/TeamComp.tsx
@@ -1,25 +1,44 @@
-import useFormat from "@/lib/useFormat"
+import useFormat from "@/lib/useFormat";
 
 type Props = {
-    email: string,
-    status: 'Active' | 'Inactive',
-    dateJoined: string,
-    totalEarnings: number
+    email: string;
+    status: "Active" | "Inactive";
+    dateJoined: string;
+    totalEarnings: number;
+};
+
+function formatEmail(email: string): string {
+    const [localPart, domain] = email.split("@");
+    if (!localPart || !domain) return email; // Fallback in case of invalid email
+    return `${localPart.slice(0, 3)}...@${domain}`;
 }
 
-function TeamComp({email, status, dateJoined, totalEarnings}: Props) {
-  return (
-    <div className="flex border-b-1 border-gray-200 px-4 py-2 my-2 items-center justify-between">
-        <div className="block max-w-2/3">
-            <p className=" font-bold">{email} <span className={`${status == 'Active' ? 'bg-green-500/30 text-green-900' : 'bg-red-500/30 text-red-900' }  font-bold text-sm rounded-xl px-2 py-1`}>{status}</span></p>
-            <p className="text-gray-400 text-sm">{dateJoined}</p>
+function TeamComp({ email, status, dateJoined, totalEarnings }: Props) {
+    return (
+        <div className="flex border-b-1 border-gray-200 px-4 py-2 my-2 items-center justify-between">
+            <div className="block max-w-2/3">
+                <p className="font-bold">
+                    {formatEmail(email)}{" "}
+                    <span
+                        className={`${
+                            status === "Active"
+                                ? "bg-green-500/30 text-green-900"
+                                : "bg-red-500/30 text-red-900"
+                        } font-bold text-sm rounded-xl px-2 py-1`}
+                    >
+                        {status}
+                    </span>
+                </p>
+                <p className="text-gray-400 text-sm">{dateJoined}</p>
+            </div>
+            <div className="block max-w-1/3">
+                <p className="font-bold">Commission</p>
+                <p className="text-gray-400 text-sm">
+                    Kes {useFormat({ value: totalEarnings })}
+                </p>
+            </div>
         </div>
-        <div className="block max-w-1/3">
-            <p className="font-bold">Commission</p>
-            <p className="text-gray-400 text-sm">Kes {useFormat({value: totalEarnings})}</p>
-        </div>
-    </div>
-  )
+    );
 }
 
-export default TeamComp
+export default TeamComp;
